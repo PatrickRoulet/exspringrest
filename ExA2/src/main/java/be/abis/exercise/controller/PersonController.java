575cdb0a73b6906598cdf76ae549ede916d01e47
PatrickRoulet <patrick.roulet@bnpparibasfortis.com>
2020-10-12 16:13:55 +0200

    ExA2 - get all persons + add person

--- ExA2/src/main/java/be/abis/exercise/controller/PersonController.java
+++ ExA2/src/main/java/be/abis/exercise/controller/PersonController.java
@@ -1,23 +1,42 @@
 package be.abis.exercise.controller;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import be.abis.exercise.model.Person;
 import be.abis.exercise.service.PersonService;
 
 @RestController
+@RequestMapping("persons")
 public class PersonController {
 
 	@Autowired
 	PersonService personService;
 	
-	@GetMapping("persons/{id}")
+	@GetMapping("{id}")
 	public Person findPerson(@PathVariable("id") int id) {
 		return personService.findPerson(id);
 	}
-
 	
+	@GetMapping(path="")
+	public List<Person> getAllPersons(){
+		return personService.getAllPersons();
+	}
+	
+	@PostMapping(path="")
+	public void addGuest(@RequestBody Person person){
+		try {
+			personService.addPerson(person);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
