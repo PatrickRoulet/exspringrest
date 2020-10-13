4810aefdce195d58134ff60b9f2ba58ab60f37ce
PatrickRoulet <patrick.roulet@bnpparibasfortis.com>
2020-10-13 09:39:25 +0200

    Added login + change password

--- ExA2/src/main/java/be/abis/exercise/controller/PersonController.java
+++ ExA2/src/main/java/be/abis/exercise/controller/PersonController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import be.abis.exercise.exception.PersonCanNotBeDeletedException;
+import be.abis.exercise.model.Login;
 import be.abis.exercise.model.Person;
 import be.abis.exercise.service.PersonService;
 
@@ -27,6 +29,11 @@ public class PersonController {
 	public Person findPerson(@PathVariable("id") int id) {
 		return personService.findPerson(id);
 	}
+
+    @PostMapping("/login")
+    public Person login(@RequestBody Login login) {
+        return personService.findPerson(login.getEmailAddress(),login.getPassword());
+    }
 	
 	@GetMapping(path="")
 	public List<Person> getAllPersons(){
@@ -34,7 +41,7 @@ public class PersonController {
 	}
 	
 	@PostMapping(path="")
-	public void addGuest(@RequestBody Person person){
+	public void addPerson(@RequestBody Person person){
 		try {
 			personService.addPerson(person);
 		} catch (IOException e) {
@@ -42,7 +49,7 @@ public class PersonController {
 		}
 	}
 	
-	@DeleteMapping(path="{id}")
+	@DeleteMapping("{id}")
 	public void deletePerson(@PathVariable("id") int id){
 		try {
 			personService.deletePerson(id);
@@ -50,5 +57,14 @@ public class PersonController {
 			e.printStackTrace();
 		}
 	}
-	
+
+    @PutMapping("{id}")
+    public void changePassword(@PathVariable("id") int id, @RequestBody Person person) {
+        try {
+            personService.changePassword(personService.findPerson(id), person.getPassword());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
